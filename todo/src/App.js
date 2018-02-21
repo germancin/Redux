80e238205f18e90a14832fc4f6aabe198a6da49a
german gonzalez <gcomlnk@gmail.com>
2018-02-21 03:13:37 -0500

    importing component Todo

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+// import ReactDOM from 'react-dom';
+// import {createStore} from 'redux';
+// import {Provider} from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
+import Todo from "./components/Todo";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">ToDo - React & Redux</h1>
+                </header>
+
+                <Todo/>
+            </div>
+        );
+    }
 }
 
 export default App;
